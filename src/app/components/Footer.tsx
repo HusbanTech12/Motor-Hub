f81a82b160a6c3c6256f5f4fee9970005acac5df
HusbanTech12 <husbantech08@gmail.com>
2025-10-30 00:11:09 +0500

    Completed Navbar

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const Footer = () => {
   return (
-    <div>
+    <>
+    
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100 bg-blue-50 '>
+     
+     <div className='flex max-md:flex-col flex=wrap justify-between gap-5 sm:px-16 px-6 py-10'> 
       
-    </div>
+      <div className='flex flex-col justify-start items-start gap-6'>
+        <Image 
+               src='/images/hero-car.webp'
+               alt='Logo'
+               width={118}
+               height={18}
+               className='object-contain '
+               />
+          <p className='text-base text-gray-700'>
+            
+            MotorHub 2025 <br />
+            All right Reserved &copy;
+            </p>     
+              
+      </div>
+
+     </div>
+
+    </footer>
+    
+    </>
   )
 }
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
+import CustomButton from './CustomButton'
 const Navbar = () => {
   return (
 
-    // Container
-    <div>
-      <h1 className=''></h1>
-    </div>
+    <>
+    <header className='w-full absolute z-10'>
+      <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
+      
+       <Link href='/' className='flex items-center  '> 
+       <Image 
+       src='/images/hero-car.webp'
+       alt='Motor-Hub Image'
+       width={118}
+       height={18}
+       className='object-contain '
+       />
+      
+       </Link>
+      
+      <CustomButton 
+      title='Sign In'
+      containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+      btnType = 'button'
+      
+      />
+      </nav>
+
+    </header>
+    
+    
+    </>
+    
   )
 }
 
